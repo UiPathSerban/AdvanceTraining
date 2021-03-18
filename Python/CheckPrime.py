6af9b3a86e6eee6e0bd65707b5042c81a185b1dd
UipathSerban <serban.popa@uipath.com>
2021-03-18 13:41:59 +0200

    init Commit

---
+++ Python/CheckPrime.py
@@ -0,0 +1,11 @@
+def primeTest (num):
+	if num > 1:
+		for i in range (2,num):
+			if (num % i) ==0:
+				isPrime = "{0} is not a prime number.".format(num)
+				break
+		else:
+			isPrime = "{0} is a prime number.".format(num)
+	else:
+		isPrime = "{0} is not a prime number.".format(num)
+	return isPrime
